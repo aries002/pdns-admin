bb6b639872cbfcc6c5886aebbeef53416676a438
aries002 <47821544+aries002@users.noreply.github.com>
2020-04-05 16:32:20 +0700

    2

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,4 +10,13 @@ class Home extends CI_Controller {
 		$this->load->view('home', $data);
 		$this->load->view('Footer');
 	}
+
+	function records()
+	{
+		$data['title'] = $this->config->item('title').' records';
+		$this->load->view('header', $data);
+		$this->load->view('navbar', $data);
+		$this->load->view('records', $data);
+		$this->load->view('Footer');
+	}
 }
